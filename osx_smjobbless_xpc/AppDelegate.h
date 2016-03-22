69ea47170684e3c22d67e44cf563bbcf9831bbf3
Nicolò Scarpa <nicolo.scarpa.work@gmail.com>
2016-03-22 19:40:37 +0100

    Initial Commit

---
+++ osx_smjobbless_xpc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  osx_smjobbless_xpc
+//
+//  Created by Studio on 22/03/16.
+//  Copyright © 2016 Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ osx_smjobbless_xpc/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  osx_smjobbless_xpc
+//
+//  Created by Studio on 22/03/16.
+//  Copyright © 2016 Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
