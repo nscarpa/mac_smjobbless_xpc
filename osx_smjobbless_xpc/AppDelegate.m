aea528ca558985c814ff8cb863388124a3e7cc9a
Nicolò Scarpa <nicolo.scarpa.work@gmail.com>
2016-03-22 21:19:14 +0100

    Uses the C XPC Services API to communicate with the host application - Adds a necessary row to Launchd.plist: "MachServices" dictionary with key set to the helper label and a boolean value set to true

--- osx_smjobbless_xpc/AppDelegate.m
+++ osx_smjobbless_xpc/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "JobBlesser.h"
 
+NSString *const HelperLabel = @"it.nicoloscarpa.studio.privileged-helper";
+
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
@@ -22,7 +24,7 @@
     NSError *error = nil;
     BOOL result = NO;
 
-    result = [jobBlesser blessHelperWithLabel:@"it.nicoloscarpa.studio.privileged-helper" error:&error];
+    result = [jobBlesser blessHelperWithLabel:HelperLabel error:&error];
     
     if (!result) {
         NSLog(@"Something went wrong! %@ / %d", [error domain], (int) [error code]);
@@ -31,6 +33,50 @@
     }
     
     NSLog(@"Job is available!");
+    
+    [self communicateWithHelper];
+}
+
+- (void)communicateWithHelper {
+    const char* service_name = [HelperLabel UTF8String];
+    
+    xpc_connection_t connection = xpc_connection_create_mach_service(service_name, NULL, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
+    
+    if (!connection) {
+        NSLog(@"Failed to create XPC connection.");
+        
+        return;
+    }
+    
+    xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
+        xpc_type_t type = xpc_get_type(event);
+        
+        if (type == XPC_TYPE_ERROR) {
+            if (event == XPC_ERROR_CONNECTION_INTERRUPTED) {
+                NSLog(@"XPC connection interupted.");
+            } else if (event == XPC_ERROR_CONNECTION_INVALID) {
+                NSLog(@"XPC connection invalid, releasing.");
+                // xpc_release(connection); // not allowed in ARC
+            } else {
+                NSLog(@"Unexpected XPC connection error.");
+            }
+        } else {
+            NSLog(@"Unexpected XPC connection event.");
+        }
+    });
+    
+    xpc_connection_resume(connection);
+    
+    xpc_object_t message = xpc_dictionary_create(NULL, NULL, 0);
+    const char* request = "Hi there, helper service.";
+    xpc_dictionary_set_string(message, "request", request);
+    
+    NSLog(@"%@", [NSString stringWithFormat:@"Sending request: %s", request]);
+    
+    xpc_connection_send_message_with_reply(connection, message, dispatch_get_main_queue(), ^(xpc_object_t reply) {
+        const char* response = xpc_dictionary_get_string(reply, "response");
+        NSLog(@"%@", [NSString stringWithFormat:@"Received response: %s.", response]);
+    });
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
--- privileged-helper/main.m
+++ privileged-helper/main.m
@@ -7,12 +7,58 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <xpc/xpc.h>
+
+static const char* helper_name = "it.nicoloscarpa.studio.privileged-helper";
+
+static void __XPC_Peer_Event_Handler(xpc_connection_t connection, xpc_object_t event) {
+    xpc_type_t type = xpc_get_type(event);
+    
+    if (type == XPC_TYPE_ERROR) {
+        if (event == XPC_ERROR_CONNECTION_INVALID) {
+            
+        } else if (event == XPC_ERROR_TERMINATION_IMMINENT) {
+            
+        }
+        
+        return;
+    }
+    
+    xpc_connection_t remote = xpc_dictionary_get_remote_connection(event);
+    
+    xpc_object_t reply = xpc_dictionary_create_reply(event);
+    const char* response = "Hi there, host application!";
+    xpc_dictionary_set_string(reply, "response", response);
+    
+    xpc_connection_send_message(remote, reply);
+    
+    // xpc_release(remote); // not allowed in ARC
+}
+
+static void __XPC_Connection_Hanlder(xpc_connection_t connection) {
+    xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
+        __XPC_Peer_Event_Handler(connection, event);
+    });
+    
+    xpc_connection_resume(connection);
+}
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        sleep(10);
-        
-        NSLog(@"Hello, World!");
+    xpc_connection_t service = xpc_connection_create_mach_service(helper_name, dispatch_get_main_queue(), XPC_CONNECTION_MACH_SERVICE_LISTENER);
+    
+    if (!service) {
+        exit(EXIT_FAILURE);
     }
-    return 0;
+    
+    xpc_connection_set_event_handler(service, ^(xpc_object_t connection) {
+        __XPC_Connection_Hanlder(connection);
+    });
+    
+    xpc_connection_resume(service);
+    
+    dispatch_main();
+    
+    // xpc_release(service); // not allowed in ARC
+    
+    return EXIT_SUCCESS;
 }
